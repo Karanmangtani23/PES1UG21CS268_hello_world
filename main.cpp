5a81a8853176983f51f8ba9c2dbaffd144ddbdcc
Karan Mangtani <151711047+Karanmangtani23@users.noreply.github.com>
2024-03-08 19:02:39 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, world! \n"<< "This is PES1UG21CS268" << endl;
+    cout << "Hello, world! \n"<< "This is PES1UG21CS268 " << endl;
     return 0;
 }
